ffb93e18ae078ae417cb61ea1243a8e04e8d05ab
smajaas <smajaas@gmail.com>
2023-05-17 23:58:55 +0530

    first query

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ Open server directory.
 
 - run "npm install" and "npm start"
 
-#### Node Course
-
-[Node Tutorial and Projects Course](https://www.udemy.com/course/nodejs-tutorial-and-projects-course/?referralCode=E94792BEAE9ADD204BC7)
-
 #### Starter
 
 - run "npm install" and "npm run dev"
--- src/Items.jsx
+++ src/Items.jsx
@@ -1,5 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
+import customFetch from './utils';
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/'),
+  });
+  console.log(result);
   return (
     <div className='items'>
       {items.map((item) => {
