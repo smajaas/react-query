ea30ade8fd3bad2ffff195f5f9a023991d81b52d
smajaas <smajaas@gmail.com>
2023-05-18 16:41:03 +0530

    use mutation and toastify helpers

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
 import customFetch from './utils';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
+  const queryClient = useQueryClient();
 
   const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (taskTitle) => customFetch.post('/', { title: taskTitle }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+      toast.success('task added');
+      setNewItemName('');
+    },
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    },
   });
 
   const handleSubmit = (e) => {
